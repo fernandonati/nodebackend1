85388b815716c0d746a4761c63998eca790c14e4
fernandonati <teste@teste.com>
2020-03-27 19:31:42 -0300

    Added celebrate Validation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express'); //add express
 const cors    = require('cors'); //cors. 
+const { errors } = require('celebrate');
 const routes  = require('./routes'); //nned put ./ to identify a file.
 
 const app = express(); //express instance
 app.use(cors()); //accept all conection, modify to host later.ex:cors({origin:'http://myhost.com'})
 app.use(express.json()); //configure to accep json.
 app.use(routes); //configure to routes instance
+app.use(errors());
 
 
 app.listen(3333); //server listen on port...
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController =  require('./controllers/IncidentController');
 const ProfileController =  require('./controllers/ProfileController');
@@ -8,13 +9,42 @@ const routes = express.Router();
 //all routes logic inside a controller.
 //****************** ONG ***************************
 routes.get('/ongs',OngController.index);  //get all data 
-routes.post('/ongs', OngController.create); //insert a new record.
+
+
+
+routes.post('/ongs',celebrate({ //insert a new record. //added celebrate validation.
+  [Segments.BODY] : Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}),OngController.create); 
+
 //****************** INCIDENT ***************************
-routes.get('/incidents',IncidentController.index);
+routes.get('/incidents',celebrate({
+    [Segments.QUERY] : Joi.object().keys({
+        page: Joi.number(),
+    })
+}),IncidentController.index);
+
 routes.post('/incidents',IncidentController.create);
-routes.delete('/incidents/:id',IncidentController.delete);
+
+routes.delete('/incidents/:id',celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}),IncidentController.delete); 
+
+
 //****************** PROFILE ***************************
-routes.get('/profile',ProfileController.index);
+routes.get('/profile',celebrate({
+  [Segments.HEADERS] : Joi.object({
+      authorization: Joi.string().required(),
+  }).unknown(), //necessary because anothers headers params.
+}) ,ProfileController.index);
+
 
 //****************** LOGIN ***************************
 routes.post('/sessions',SessionController.create);
